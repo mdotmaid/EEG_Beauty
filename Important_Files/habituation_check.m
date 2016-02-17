28c69bf24d69621fd941a71425a13c14b8265488
mdotmaid <Michelle.Maiden@colorado.edu>
2016-02-16 18:59:39 -0700

    Update habituation_check.m

--- Important_Files/habituation_check.m
+++ Important_Files/habituation_check.m
@@ -1,5 +1,5 @@
-function [goodinds, goodpks]  =  habituation_check(deviant,standard,localt,samplerate,interval);
-% A function by Michelle Maiden
+function [goodinds, goodpks]  =  habituation_check(deviant,standard,localt,...
+                                                samplerate,offset,win);
         [m,n] = size(deviant);
         stimpks  = zeros(1,m);
         ctr = 0;
@@ -7,8 +7,10 @@ function [goodinds, goodpks]  =  habituation_check(deviant,standard,localt,sampl
         
     for si = 1:m
         ctr = ctr+1;
-        stim3  = meanclean(deviant(si:si,:),samplerate);
-        [sval,sloc] = mmn(deviant,standard,localt(1));
+        % Average over all epochs within interval of si
+        fe  = find(offset<(offset(si)+win),1,'last');
+        stim3  = meanclean(deviant(si:fe,:),samplerate);
+        [sval,sloc] = mmn(stim3,standard,localt(1));
         if isempty(sval) || sval>0 || sval<-15
             stimpks(ctr) = NaN;
             badinds      = [badinds, si];
@@ -23,8 +25,20 @@ function [goodinds, goodpks]  =  habituation_check(deviant,standard,localt,sampl
     %             continue;
     %         end
     end
-    goodinds = setxor(1:m,intersect(badinds,1:m));
+    if isempty(badinds)
+        goodinds = 1:m;
+    else
+        goodinds = setxor(1:m,intersect(badinds,1:m));
+    end
+    if isempty(si)
+        si = m;
+    end
     goodinds(goodinds>si)=[];
     goodpks  = stimpks(goodinds);
+    disp([sprintf('\t\tHabituation Time: '),num2str(offset(si)/1000/60),...
+          ' min out of ',num2str(offset(m)/1000/60),' min']);
+    disp([sprintf('\t\t'),num2str(length(badinds)),...
+         ' additional epochs rejected in moving window average']);
+
 end
     
